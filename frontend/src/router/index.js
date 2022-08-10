d0798a26a0e8d0966d2eebf09cd4cb229690d826
JH_Kim <jimjack@naver.com>
2022-08-10 17:25:54 +0900

    QNA 목록 페이지 및 QNA 문의글 작성 페이지 라우터 등록
    제곧내

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -14,6 +14,16 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/service/qna',
+    name: 'qna',
+    component: QnAView
+  },
+  {
+    path: '/service/qna/write',
+    name: 'qna-write',
+    component: QnAWriteView
   }
 ]
 
