9cb10f98fa3c1599636f533cc249123d5781a009
JH_Kim <jimjack@naver.com>
2022-08-10 17:24:59 +0900

    기본 텍스트 에디터 컴포넌트 및 공용 입력 컴포넌트 작성
    텍스트 에디터(TextEditor.vue)
    VueQuill을 사용, 필요한 부분만 사용할 수 있도록 래핑
    
    공용 입력 컴포넌트(CommonWrite.vue)
    말 그대로 외관만 작성
    제목 입력란 및 드롭다운 메뉴는 Vuetify, 텍스트 에디터는 위의 TextEditor 사용
    카테고리, 작성, 취소 기능은 추후 BE 완성한 뒤 구현

---
+++ frontend/src/components/TextEditor.vue
@@ -0,0 +1,54 @@
+<!--
+  220810 김주현
+  Vue Quill Editor를 사용한 텍스트 에디터입니다.
+  높이는 600px로 잡혀있습니다.
+
+  [ 이벤트 ]
+  onChangeContent(html :String)
+    텍스트 에디터 내 값이 바뀔 때마다 Trigger 됩니다.
+    html 변수를 통해 에디터 내 값을 취득할 수 있습니다.
+    사용 예시는 CommonBoard.vue 컴포넌트를 참고해주시기 바랍니다람쥐
+-->
+
+<template>
+  <div class="texteditor">
+    <quill-editor
+      ref="textEditor"
+      :toolbar="'full'"
+      :placeholder="placeholder"
+      @textChange="onTextChange">
+    </quill-editor>
+  </div>
+</template>
+<script>
+  import { QuillEditor } from '@vueup/vue-quill'
+  import '@vueup/vue-quill/dist/vue-quill.snow.css';
+
+  export default
+  {
+    name: 'TextEditor',
+    components: {
+      QuillEditor
+    },
+    props: {
+      placeholder: String
+    },
+    methods: {
+      onTextChange() {
+        let html = this.$refs.textEditor.getHTML();
+        this.$emit('onChangeContent', html);
+      }
+    }
+  }
+</script>
+
+<style scope>
+  .texteditor {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    height: 600px;
+    border: 1px solid black;
+  }
+
+</style>
