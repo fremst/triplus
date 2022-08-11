268a8775e4bba8fc0c287c414a243a1e13934d58
Mingi1029 <alsrl1406@naver.com>
2022-08-11 17:56:50 +0900

    TP-76 [fe]공지사항 목록, 등록 레이아웃
    공지사항 목록 Vuetify table 사용해서 기본 디자인
    공지사항 등록 간단한 Form 디자인처리

---
+++ frontend/src/views/admin/service/NoticeList.vue
@@ -0,0 +1,85 @@
+<template>
+<!--    <div class="home">-->
+<!--        <CommonBoard :detailLink="detailLink" :board="board"/>-->
+<!--    </div>-->
+    <div id="main">
+    <v-table>
+        <thead>
+        <tr>
+            <th class="text-left">
+                Number
+            </th>
+            <th class="text-left">
+                Title
+            </th>
+            <th class="text-left">
+                Id
+            </th>
+            <th class="text-left">
+                Date
+            </th>
+            <th class="text-left">
+                View
+            </th>
+
+        </tr>
+        </thead>
+        <tbody>
+        <tr
+                v-for="item in board"
+                :key="item.number"
+        >
+            <td>{{ item.number }}</td>
+            <td>{{ item.title }}</td>
+            <td>{{ item.id }}</td>
+            <td>{{ item.date }}</td>
+            <td>{{ item.view }}</td>
+        </tr>
+        </tbody>
+    </v-table>
+    </div>
+</template>
+
+<script>
+    // @ is an alias to /src
+//    import CommonBoard from '@/components/CommonBoard.vue'
+
+    export default {
+        name: 'NoticeListView',
+        // components: {
+        //     CommonBoard
+        // },
+        data() {
+            return {
+                detailLink: "/service/notice",
+                board: {
+                    title: "공지사항",
+                    list: [
+                        {
+                            number: 1,
+                            title: "제목",
+                            id: "작성자",
+                            date: "20:14",
+                            view: 10
+                        }, {
+                            number: 2,
+                            title: "bbb",
+                            id: "id 2",
+                            date: "2022.08.01",
+                            view: 3
+                        },
+
+                    ],
+                    pageCur: 5, pageEnd: 15
+                }
+            };
+        }
+    }
+</script>
+<style scoped>
+    #main{
+        width:1060px;
+        height: 1500px;
+        margin: auto;
+    }
+</style>
---
+++ frontend/src/views/admin/service/NoticeWrite.vue
@@ -0,0 +1,63 @@
+<template>
+<!--    <div class="home">-->
+<!--        <CommonWrite :title="title" :submitLink="submitLink" :cancelLink="cancelLink"/>-->
+<!--    </div>-->
+    <div id="main">
+            <div class="container">
+                <h1>공지사항 등록</h1>
+                <form action="" method="post">
+                    <v-form>
+                        제목<br>
+                        <input type="text" class="form-control" id="title" placeholder="제목 입력" name="title">
+                    </v-form>
+                    <br>
+
+                    <v-form>
+                        작성자<br>
+                        <input type="text" class="form-control" id="id" name="id" value="admin"
+                                                                   readonly="readonly">
+                    </v-form>
+                    <br>
+
+                    <v-form>
+                        내용<br>
+                        <v-text-field class="form-control" rows="10" id="contents"
+                                  name="contents" placeholder="내용 작성(최대 500자)"></v-text-field>
+                    </v-form>
+                    <br> <br>
+                    <v-btn id="btn1">등록</v-btn>
+                    <v-btn id="btn1">취소</v-btn>
+                </form>
+            </div>
+
+    </div>
+</template>
+
+<script>
+    //  @ is an alias to /src
+    // import CommonWrite from '@/components/CommonWrite.vue'
+
+    export default {
+        name: 'NoticeWriteView',
+        // components: {
+        //     CommonWrite
+        // },
+        data() {
+            return {
+                title: "이곳에 타이틀 입력",
+                submitLink: "/service/notice/write",
+                cancelLink: "/service/notice",
+            };
+        }
+    }
+</script>
+<style scoped>
+    #main{
+        width:1060px;
+        height: 1500px;
+        margin: auto;
+    }
+    #btn1{
+        margin-right: 10px;
+    }
+</style>
